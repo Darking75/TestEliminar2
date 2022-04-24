6e3c57dcdbf938f6ac84fd71c521b98808a108b5
Alejandro <torresxiaotorres@hotmail.com>
2022-04-24 06:59:16 -0500

    Initial Commit

---
+++ TestEliminar2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestEliminar2
+//
+//  Created by Alejanro Cardenas on 24/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
